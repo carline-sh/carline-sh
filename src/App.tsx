bae262778a2b7afdb46c17f21d5410fca2cdd142
carline <carline@v3x.email>
2025-10-21 20:17:51 +0200

    update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div className="bg-blue-200">hi!</div>;
+  return (
+    <div className="min-h-screen">
+      <div className="w-full max-w-xl mx-auto">
+        <div className="bg-blue-200 p-12 flex justify-center">hi!</div>
+        <div className="bg-blue-100">info</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
